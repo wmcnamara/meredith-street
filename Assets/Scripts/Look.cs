8489e3ca98a8db91426ec170521681faef4b2e89
Weston McNamara <westonfmcnamara@gmail.com>
2021-10-13 16:16:11 -0400

    Initial unity commit

---
+++ Assets/Scripts/Look.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Gives the player the ability to use the mouse to rotate a player object in a first person fashion.
+
+    This should be attached to an object with a camera child object. It will rotate the entire object horizontally, and the camera vertically. 
+*/
+
+public class Look : MonoBehaviour
+{
+    [SerializeField] private float sensitivity = 1.0f;
+    [SerializeField] private Transform cameraTransform = null;
+
+    public bool blockRot = false; //If true no rotation will be applied to the player
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnDestroy()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    float yRot = 0.0f;
+
+    // Calling this in LateUpdate prevents jitter while moving
+    void LateUpdate()
+    {
+        if (blockRot)
+            return;
+
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        yRot -= mouseY;
+        yRot = Mathf.Clamp(yRot, -90, 90);
+
+        transform.Rotate(Vector3.up * mouseX);
+        cameraTransform.transform.localRotation = Quaternion.Euler(yRot, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Moves the object this is attached to with a Character controller and the WASD keys. 
+*/
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float walkingSpeed = 5.0f;
+    [SerializeField] private float runningSpeed = 7.0f;
+    [SerializeField] private float gravity = -9.0f;
+
+    private CharacterController controller;
+    bool shouldSleepMovement = false;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    float gDisp = 0.0f;
+    void Update()
+    {
+        if (shouldSleepMovement)
+            return;
+
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        //use running speed if holding shift
+        float movementSpeed = Input.GetKey(KeyCode.LeftShift) ? runningSpeed : walkingSpeed; 
+
+        //gravity
+        if (!controller.isGrounded)
+            gDisp += (0.5f * gravity) * Time.deltaTime * Time.deltaTime;
+        else
+            gDisp = 0.0f;
+
+        //keyboard movement
+        Vector3 xMov = transform.right * xInput;
+        Vector3 yMov = transform.forward * yInput;
+
+        Vector3 move = xMov + yMov;
+
+        if (move.magnitude > 1)
+            move.Normalize();
+
+        move *= movementSpeed * Time.deltaTime;
+        move.y = gDisp;
+
+        controller.Move(move);
+    }
+
+    //Prevents movement for a specified amount of seconds
+    //Gravity is NOT applied
+    public void SleepMovementSeconds(float seconds)
+    {
+        StartCoroutine(SleepMov(seconds));
+    }
+
+    //Internal sleep movement coroutine
+    private IEnumerator SleepMov(float seconds) 
+    {
+        shouldSleepMovement = true;
+        yield return new WaitForSeconds(seconds);
+        shouldSleepMovement = false;
+    }
+}
